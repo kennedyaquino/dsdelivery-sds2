16d8b928fdeb80fe9e6d7937befaa34347f13eb6
kennedyaquino <kennedy.eletronica.94@gmail.com>
2021-01-08 21:46:35 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/br/dsdelivery/delivery/controller/OrderController.java
+++ backend/src/main/java/com/br/dsdelivery/delivery/controller/OrderController.java
@@ -30,4 +30,10 @@ public class OrderController {
         return ResponseEntity.created(uri).body(dto);
 
     }
+
+    @PutMapping(value = "/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok(dto);
+    }
 }
