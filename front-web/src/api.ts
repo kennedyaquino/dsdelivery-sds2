41736f10fb035faf2409a7e95f4aa16fae984191
kennedyaquino <kennedy.eletronica.94@gmail.com>
2021-01-10 22:27:31 -0300

    Add server

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { OrderPayload } from './Orders/types';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://kennedy-sds2.herokuapp.com';
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts() {
@@ -9,9 +9,9 @@ export function fetchProducts() {
 }
 
 export function fetchLocalMapBox(local: string) {
-    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}
-
-`)
+    return axios(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`
+    )
 }
 
 export function saveOrder(payload: OrderPayload) {
